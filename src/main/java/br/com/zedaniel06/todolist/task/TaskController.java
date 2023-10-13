be898def7085d7b1efbadb7acb0040891ac29891
ZeroMeia123 <silvadan752@gmail.com>
2023-10-13 11:03:13 -0300

    Fazendo que só o dono de uma task possa fazer update

--- src/main/java/br/com/zedaniel06/todolist/task/TaskController.java
+++ src/main/java/br/com/zedaniel06/todolist/task/TaskController.java
@@ -52,13 +52,19 @@ public class TaskController {
     }
 
     @PutMapping("/{id}")
-    public TaskModel update(@RequestBody TaskModel taskModel, HttpServletRequest request, @PathVariable UUID id){
-        var idUser = request.getAttribute("idUser");
-
+    public ResponseEntity update(@RequestBody TaskModel taskModel, HttpServletRequest request, @PathVariable UUID id){
         var task = this.taskRepository.findById(id).orElse(null);
+        if(task==null){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Tarefa não encontrada");
+        }
+        var idUser = request.getAttribute("idUser");
+        if(!task.getIdUser().equals(idUser)){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Você só pode editar as tasks das quais é o dono.");
+        }
         Utils.copyNonNullProperties(taskModel, task);
 
-        return this.taskRepository.save(task);
+        var taskUpdated = this.taskRepository.save(task);
+        return ResponseEntity.status(HttpStatus.OK).body(taskUpdated);
 
     }
 
